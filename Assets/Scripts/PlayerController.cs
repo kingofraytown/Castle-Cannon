22e91e2500d326a27b9d55a9d3c7012673ebe4af
kingofraytown <96214075+anthonyljones26@users.noreply.github.com>
2025-08-22 22:58:00 -0400

    Added projectiles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public HealthBar powerBar;
     public float chargeRate;
     public float currentCharge;
+    public bool isFiringBullets = false;
+    public Transform turret;
 
     public enum ChargeState
     {
@@ -21,6 +23,16 @@ public class PlayerController : MonoBehaviour
 
     public ChargeState currentChargeState = ChargeState.None;
 
+    public enum HealthState
+    {
+        Full,
+        Damage1,
+        Damage2,
+        Damage3,
+        Damage4,
+        Destroyed
+    }
+    public HealthState currentHealthState = HealthState.Full;
     public Gun basicShot;
     public Gun chargeShot;
     public Gun bigChargeShot;
@@ -35,6 +47,65 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ChargeCannon();
+    }
+
+    void ChargeCannon()
+    {
+        currentCharge += chargeRate * Time.deltaTime;
+        UpdateChargeStatus(currentCharge);
         
+    }
+
+    void UpdateChargeStatus(float charge)
+    {
+        if(charge <= 10)
+        {
+            if(currentChargeState != ChargeState.None)
+            {
+                currentChargeState = ChargeState.None;
+            }
+        } else if(charge > 10 && charge <= 25)
+        {
+            if (currentChargeState != ChargeState.Basic)
+            {
+                currentChargeState = ChargeState.Basic;
+            }
+        } else if(charge > 25 && charge <= 50)
+        {
+            if (currentChargeState != ChargeState.Charge)
+            {
+                currentChargeState = ChargeState.Charge;
+            }
+        } else if (charge > 50 && charge <= 75)
+        {
+            if (currentChargeState != ChargeState.BigCharge)
+            {
+                currentChargeState = ChargeState.BigCharge;
+            }
+        }
+        else if (charge > 75 && charge <= 100)
+        {
+            if (currentChargeState != ChargeState.GreatCharge)
+            {
+                currentChargeState = ChargeState.GreatCharge;
+            }
+        } else if(charge > 100)
+        {
+            if (currentChargeState != ChargeState.OverFlow)
+            {
+                currentChargeState = ChargeState.OverFlow;
+            }
+        }
+    }
+
+    void UpdatePowerBar()
+    {
+        powerBar.health = currentCharge;
+    }
+
+    void HealthCheck()
+    {
+
     }
 }
